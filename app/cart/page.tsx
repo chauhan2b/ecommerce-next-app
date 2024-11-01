ef6242e6fb66546159624c85e1d09337b37714af
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-11-01 17:48:17 -0400

    Add header and footer with link to cart page

---
+++ app/cart/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div className="h-screen bg-gray-50">Page</div>;
+}
---
+++ app/components/header.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { ShoppingCart } from "lucide-react";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="shadow-md p-4">
+      <div className="flex justify-between items-center">
+        <h1 className="text-xl font-bold">E-commerce</h1>
+        <div className="flex flex-col gap-2">
+          <Link href="/cart" passHref>
+            <Button variant="ghost">
+              <ShoppingCart />
+              Cart
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
