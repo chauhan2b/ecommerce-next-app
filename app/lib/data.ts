625fd99afda9188fd1875cd68014e1dab3d8942a
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-11-04 16:52:43 -0500

    Add a product details page

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,16 +1,35 @@
-import { Product } from "@/app/lib/definitions";
+import { Product, ProductDetails } from "@/app/lib/definitions";
 
 export async function fetchProducts() {
   try {
     const response = await fetch(
       "https://dummyjson.com/products?select=id,title,thumbnail,category,price,discountPercentage,rating"
     );
-    const data = await response.json();
-    const products = data.products as Product[];
 
-    return products;
+    if (!response.ok) {
+      throw new Error(`HTTP error: Status code ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.products as Product[];
   } catch (error) {
     console.log("Failed to fetch products", error);
     throw new Error("Failed to fetch products");
+  }
+}
+
+export async function fetchProductDetails(id: string): Promise<ProductDetails> {
+  try {
+    const response = await fetch(`https://dummyjson.com/products/${id}`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error: Status code ${response.status}`);
+    }
+
+    const productDetails = await response.json();
+    return productDetails as ProductDetails;
+  } catch (error) {
+    console.log("Failed to fetch product details", error);
+    throw new Error("Failed to fetch product details");
   }
 }
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,10 +1,150 @@
-"use client";
+import { fetchProductDetails } from "@/app/lib/data";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Heart, Star, Truck } from "lucide-react";
+import Image from "next/image";
 
-import { useParams } from "next/navigation";
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id?: string }>;
+}) {
+  const id = (await params).id;
 
-export default function Page() {
-  const params = useParams();
-  const { id } = params;
+  if (!id) {
+    return <div className="h-screen">Invalid product ID</div>;
+  }
 
-  return <div>Products Page for {id}</div>;
+  const product = await fetchProductDetails(id! as string);
+  const discountedPrice = (
+    product.price -
+    (product.price * product.discountPercentage) / 100
+  ).toFixed(2);
+
+  const formatDate = (dateString: string) => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+
+  return (
+    <div>
+      <div className="grid md:grid-cols-2 justify-center xl:px-40 md:max-h-min">
+        <div>
+          <Image
+            src={product.images[0]}
+            width={500}
+            height={500}
+            alt={product.title}
+            className="w-full rounded-lg lg:max-w-[700px]"
+          />
+        </div>
+        <div className="p-2 md: pl-6 gap-4 flex flex-col lg:min-w-[500px] lg:max-w-[650px]">
+          <h1 className="text-3xl font-bold md:mt-20 xl:mt-32">
+            {product.title}
+          </h1>
+          <div className="flex gap-2 items-center ">
+            <Star className="w-5 h-5 text-yellow-300" fill="currentColor" />
+            {product.rating}
+            <p className="pl-2 text-gray-500">
+              {product.reviews.length} reviews
+            </p>
+            <p>{product.stock} in stock</p>
+          </div>
+          <div>
+            <p className="text-gray-600 line-through text-sm">
+              ${product.price}
+            </p>
+            <div className="flex gap-2 items-center">
+              <p className="text-2xl font-bold">${discountedPrice}</p>
+              <p className="bg-green-600 px-2 py-1 rounded-lg text-white font-bold text-sm">
+                {product.discountPercentage}% off
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-2">
+            <Button className="w-full p-6 text-xl font-bold">
+              Add to cart
+            </Button>
+            <Button className="p-6" variant="outline">
+              <Heart className="h-6 w-6" />
+            </Button>
+          </div>
+
+          <div className="flex gap-2 items-center text-gray-500">
+            <Truck />
+            <p>{product.shippingInformation}</p>
+          </div>
+          <div className="hidden lg:block">
+            <div className="flex flex-col gap-2">
+              <h1 className="text-xl font-bold mt-2">Description</h1>
+              <p className="text-lg text-muted-foreground">
+                {product.description}
+              </p>
+            </div>
+            <div className=" flex flex-col gap-2">
+              <h1 className="text-xl font-bold mt-2">Tags</h1>
+              {product.tags.length > 0 && (
+                <div className="flex gap-2">
+                  {product.tags.map((tag) => (
+                    <Badge
+                      key={tag}
+                      className="text-sm bg-gray-100 text-black font-normal hover:text-white"
+                    >
+                      {tag}
+                    </Badge>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="px-6 lg:hidden md:mx-20">
+        <div className=" flex flex-col gap-2">
+          <h1 className="text-xl font-bold mt-2">Description</h1>
+          <p className="text-lg text-muted-foreground">{product.description}</p>
+        </div>
+        <div className=" flex flex-col gap-2">
+          <h1 className="text-xl font-bold mt-2">Tags</h1>
+          {product.tags.length > 0 && (
+            <div className="flex gap-2">
+              {product.tags.map((tag) => (
+                <Badge
+                  key={tag}
+                  className="text-sm bg-gray-100 text-black font-normal hover:text-white"
+                >
+                  {tag}
+                </Badge>
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="flex flex-col gap-2 md:mx-20 xl:mx-40 px-6 pb-4">
+        <h1 className="text-xl font-bold mt-4">Reviews</h1>
+        <div className="grid md:grid-cols-2 gap-4">
+          {product.reviews.map((review) => (
+            <div
+              key={review.reviewerEmail}
+              className="bg-gray-50 p-4 rounded-lg"
+            >
+              <div className="flex justify-between">
+                <p className="text-md font-medium">{review.reviewerName}</p>
+                <p className="text-gray-600">{formatDate(review.date)}</p>
+              </div>
+              <div className="flex gap-2">
+                <Star className="w-5 h-5 text-yellow-300" fill="currentColor" />
+                <p>{review.rating}</p>
+              </div>
+              <p className="text-gray-500">{review.comment}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
