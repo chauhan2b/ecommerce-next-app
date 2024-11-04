4df2e359eb8ecf07f7525152baec78305740380d
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-11-04 13:16:11 -0500

    Add website title in footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,3 +1,11 @@
 export default function Footer() {
-  return <div className="bg-gray-50">Footer</div>;
+  return (
+    <div className="bg-gray-50">
+      <div className="container mx-auto p-6">
+        <div className="text-center text-gray-500">
+          <p>© 2021 Next.js E-commerce</p>
+        </div>
+      </div>
+    </div>
+  );
 }
