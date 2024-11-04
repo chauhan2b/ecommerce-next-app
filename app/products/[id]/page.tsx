2a2804f244bf85ced8772d6365eddd2dc7672eed
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-11-04 17:14:08 -0500

    Show email in reviews

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -129,15 +129,25 @@ export default async function Page({
         <div className="grid md:grid-cols-2 gap-4">
           {product.reviews.map((review, index) => (
             <div key={index} className="bg-gray-50 p-4 rounded-lg">
-              <div className="flex justify-between">
-                <p className="text-md font-medium">{review.reviewerName}</p>
+              <div className="flex justify-between items-center">
+                <div>
+                  <div className="flex gap-2">
+                    <p className="text-md font-medium">{review.reviewerName}</p>
+                    <div className="flex gap-2">
+                      <Star
+                        className="w-5 h-5 text-yellow-300"
+                        fill="currentColor"
+                      />
+                      <p>{review.rating}</p>
+                    </div>
+                  </div>
+                  <p className="text-gray-500 text-sm">
+                    {review.reviewerEmail}
+                  </p>
+                </div>
                 <p className="text-gray-600">{formatDate(review.date)}</p>
               </div>
-              <div className="flex gap-2">
-                <Star className="w-5 h-5 text-yellow-300" fill="currentColor" />
-                <p>{review.rating}</p>
-              </div>
-              <p className="text-gray-500">{review.comment}</p>
+              <p className="text-gray-700 mt-2">{review.comment}</p>
             </div>
           ))}
         </div>
