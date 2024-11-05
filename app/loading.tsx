d1f68d3966c5021b18d5d8603ee59a3a7299b8cb
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-11-05 18:41:44 -0500

    Add a loading page

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+  return (
+    <div
+      className="flex items-center justify-center"
+      style={{ height: "calc(100vh - 8rem)" }}
+    >
+      Loading
+    </div>
+  );
+}
