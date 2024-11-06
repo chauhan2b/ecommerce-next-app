f04cff32946f2ddc86a13c8b5ec19edf3842d76d
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-11-06 11:35:33 -0500

    Fix loading screen bug

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,5 @@
 export default function Loading() {
   return (
-    <div
-      className="flex items-center justify-center"
-      style={{ height: "calc(100vh - 8rem)" }}
-    >
-      Loading
-    </div>
+    <div className="flex items-center justify-center h-screen">Loading...</div>
   );
 }
